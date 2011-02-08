027067ddd7c63acadea331c8fec368149c694302
pszmyd <pszmyd@localhost>
2011-02-08 01:32:46 +0100

    Initial source code upload

---
+++ Handlers/ShareBarPartHandler.cs
@@ -0,0 +1,9 @@
+﻿using JetBrains.Annotations;
+using Orchard.ContentManagement.Handlers;
+
+namespace Szmyd.Orchard.Modules.Sharing.Handlers
+{
+    [UsedImplicitly]
+    public class ShareBarPartHandler : ContentHandler
+    {}
+}
---
+++ Settings/ShareBarMode.cs
@@ -0,0 +1,17 @@
+﻿
+namespace Szmyd.Orchard.Modules.Sharing.Settings
+{
+    /// <summary>
+    /// AddThis share bar basic display modes
+    /// </summary>
+    public enum ShareBarMode
+    {
+        TweetLikeShare,
+        VerticalShareCounter,
+        HorizontalShareCounter,
+        ToolboxFacebookLike,
+        MediumIcons,
+        RetweetLikeShare,
+        Css3StyleButtons
+    }
+}
